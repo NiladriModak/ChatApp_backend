54a0d512922fa89f5f204eb9ab09d122eddbe07d
NiladriModak <niladrimodakk2001@gmail.com>
2024-03-07 14:18:50 +0530

    second

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,11 +1,11 @@
-const mongoose=require("mongoose")
-const connectdb=()=>{
-    mongoose.connect("mongodb://localhost:27017",{
-        dbName:"EConnect"
-    }).then(
-        (data)=>{
-            console.log("Connected to db ",data.connection.host)
-        }
-    )
-}
-module.exports=connectdb
+const mongoose = require("mongoose");
+const connectdb = () => {
+  mongoose
+    .connect(process.env.MONGODBURL, {
+      dbName: "EConnect",
+    })
+    .then((data) => {
+      console.log("Connected to db ", data.connection.host);
+    });
+};
+module.exports = connectdb;
